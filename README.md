0e5b9f931e65979bcbd82b0be4cacc266501ae96
James Poole <14151626@studentmail.ul.ie>
2018-05-10 17:49:10 +0100

    first tunings
    some mp3s uploaded
    standard and dadgad tunings supported

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 ## Tuner
-Use Mycroft as a tuning fork
+A skill for the Mycroft AI platform that enables tuning fork functionality
 
 ## Description
-Play notes or full tunings to help you tune your instruments
+Ask Mycroft to play a tuning or to play an individual note. Does what it says on the tin.
 
 ## Examples
  * "Tune to standard."
- * "Play the note E."
+ * "Tune to dadgad"
 
 ## Credits
 JamesPoole
 
+## Features
+_This skill is under development and is still not yet fully functional._
+
+### Supported Tunings
+1. Standard
+2. DADGAD
--- __init__.py
+++ __init__.py
@@ -1,15 +1,39 @@
+from os.path import dirname, join
+import time
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
+from mycroft.util import play_mp3
+from mycroft.util.log import getLogger
+from mycroft.audio import wait_while_speaking
 
+LOGGER = getLogger(__name__)
 
 class TunerSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder().require('Tuner'))
-    def handle_tuner(self, message):
-        self.speak_dialog('tuner')
+    def play_tuning(self, tuning, notes):
+        self.load_data_files(dirname(__file__))
+        self.speak("Playing the notes for " + tuning)
+        wait_while_speaking()
+        for note in notes:
+            LOGGER.info(note)
+            title = note + ".mp3" 
+            self.process = play_mp3(join(dirname(__file__), "mp3s", title))
+            time.sleep(3)
 
+    @intent_handler(IntentBuilder("TuningsIntent").require('Tuner'))
+    def handle_play_tuning(self, message):
+        utter = message.data['utterance']
+        tuning_names = ['standard', 'dadgad']
+        tuning_notes = {
+                'standard': ['E', 'A', 'D', 'G', 'B', 'E_high'],
+                'dadgad': ['D', 'A', 'D', 'G', 'A', 'D']
+        }
+
+        for tuning in tuning_names:
+            if tuning in utter:
+               self.play_tuning(tuning, tuning_notes[tuning]) 
 
 def create_skill():
     return TunerSkill()
