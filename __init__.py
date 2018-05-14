29a26363a7e09b6d8c58f21bb01577945db1e041
James Poole <14151626@studentmail.ul.ie>
2018-05-14 23:37:49 +0100

    change to mp3 system
    added web settings
    functionality to expand on types of instruments
    changed supported tunings

--- __init__.py
+++ __init__.py
@@ -17,23 +17,28 @@ class TunerSkill(MycroftSkill):
         self.speak("Playing the notes for " + tuning)
         wait_while_speaking()
         for note in notes:
-            LOGGER.info(note)
-            title = note + ".mp3" 
-            self.process = play_mp3(join(dirname(__file__), "mp3s", title))
+            title = note + "_" + self.settings["play_duration"] + ".mp3" 
+            self.process = play_mp3(join(dirname(__file__), 
+                self.settings["instrument_select"], title))
             time.sleep(3)
 
     @intent_handler(IntentBuilder("TuningsIntent").require('Tuner'))
     def handle_play_tuning(self, message):
         utter = message.data['utterance']
-        tuning_names = ['standard', 'dadgad']
+        tuning_names = ['standard', 'drop d', 'half step down']
         tuning_notes = {
-                'standard': ['E', 'A', 'D', 'G', 'B', 'E_high'],
-                'dadgad': ['D', 'A', 'D', 'G', 'A', 'D']
+                'standard': ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
+                'drop d': ['D2', 'A2', 'D3', 'G3', 'B3', 'E4'],
+                'half step down': ['Ds2', 'Gs2', 'Cs3', 'Fs3', 'As3', 'Ds4']
         }
 
         for tuning in tuning_names:
             if tuning in utter:
-               self.play_tuning(tuning, tuning_notes[tuning]) 
+                self.play_tuning(tuning, tuning_notes[tuning]) 
+                return True
+
+        self.speak("That is an unsupported tuning.")
+
 
 def create_skill():
     return TunerSkill()
